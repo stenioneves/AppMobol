5023f1a048e56b4bc4ec49699938cf24e1fee393
Stenio das Neves Silva <stenionevessilva@gmail.com>
2019-08-08 21:17:32 -0300

    Arquivo gerar a view de login

---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+export default function Login(){
+    return (
+    <View style ={styles}>
+
+    </View>
+    );
+}
+const styles= StyleSheet.create({
+
+});
