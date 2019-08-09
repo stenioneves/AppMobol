b30ad65a6af9ca060a735333f3fd9ee3b0fb44c3
Stenio das Neves Silva <stenionevessilva@gmail.com>
2019-08-09 00:06:32 -0300

    Habiltado a função do botão

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,10 @@ import {View, Text,StyleSheet,Image,TextInput,TouchableOpacity} from 'react-nati
 
 import logo from '../assets/logo.png';
 
-export default function Login(){
+export default function Login( {navigation}){
+  function handleLogin(){
+    navigation.navigate('Main');
+  }
     return (
     <View style ={styles.container}>
       <Image source={logo} />
@@ -14,7 +17,7 @@ export default function Login(){
       placeholderTextColor="#999"
       style={styles.input}
       />
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity onPress={handleLogin} style={styles.button}>
           <Text style={styles.buttonText}>Entrar</Text>
       </TouchableOpacity>
     </View>
