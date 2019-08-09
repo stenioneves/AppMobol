be357eb1e0806c773c46aff5c1ea0b27431d4fe7
Stenio das Neves Silva <stenionevessilva@gmail.com>
2019-08-08 21:31:50 -0300

    Adcionado estilo na View e importado a logo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet,Image} from 'react-native';
+
+import logo from '../assets/logo.png';
 
 export default function Login(){
     return (
-    <View style ={styles}>
-
+    <View style ={styles.container}>
+      <Image source={logo} />
     </View>
     );
 }
 const styles= StyleSheet.create({
-
+   container:{
+     flex:1,
+     backgroundColor: '#f5f5f5',
+     justifyContent: 'center',
+     alignItems :'center',
+     padding: 30 ,
+   },
 });
