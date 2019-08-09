17967ac02f655c74d62ae715c464b24feef8266e
Stenio das Neves Silva <stenionevessilva@gmail.com>
2019-08-08 21:16:22 -0300

    Arquivo movido para SRC e limpado os códigos desnecessários

--- src/index.js
+++ src/index.js
@@ -1,39 +1,13 @@
 import React from 'react';
-import {
-  
-  View,
-  Text,
-  StyleSheet
-  
-} from 'react-native';
+
+import Login from './pages/Login';
 
 export default function App() {
   return (
-    <View style={styles.container }>
-    <Text style={styles.text}>Hello World !</Text>
-    <Text style={styles.text}>Hello World !</Text>
-    </View>
+   <Login />
   );
 };
 
-const styles = StyleSheet.create({
-  container:{
-   flex:1,
-   //display: 'flex',
-   //flexDirection: 'column',
-   
-   backgroundColor:'#7159c1',
-   justifyContent :'center',
-   alignItems: 'center'
-   },
-   text :{
-      fontWeight:'bold',
-      color: '#FFF',
-      fontSize: 20
-   }
 
-
-
-});
 
 
