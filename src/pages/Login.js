e2d10a9a1198b9cfc86a1668e5c0bbd44ff0a5ad
Stenio das Neves Silva <stenionevessilva@gmail.com>
2019-08-12 15:21:36 -0300

    correção da função handleLogin

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import {View, Text,StyleSheet,Image,TextInput,TouchableOpacity} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.png';
 
+
 export default function Login( {navigation}){
-  function handleLogin(){
-    navigation.navigate('Main');
+  const [user,setUser]= useState('');
+
+  useEffect(()=>{
+    AsyncStorage.getItem('user').then(user=>{
+      if(user){
+        navigation.navigate('Main',{ user })
+
+      }
+    })
+   },[]);
+  
+   
+   async function handleLogin(){
+     const response= await api.post('/devs',{username:user});
+    
+     const {_id}=response.data;
+     await AsyncStorage.setItem('user',_id);
+   
+    navigation.navigate('Main',{_id});
   }
+
+
     return (
     <View style ={styles.container}>
       <Image source={logo} />
@@ -16,6 +39,8 @@ export default function Login( {navigation}){
       placeholder="Digite o seu usuário do Github" 
       placeholderTextColor="#999"
       style={styles.input}
+      value={user}
+      onChangeText={setUser}
       />
       <TouchableOpacity onPress={handleLogin} style={styles.button}>
           <Text style={styles.buttonText}>Entrar</Text>
